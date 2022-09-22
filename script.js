0032e0c5046893420089d41cccb334338e48f1b7
Timcooper709 <112970939+Timcooper709@users.noreply.github.com>
2022-09-22 17:10:34 -0400

    card info done

--- script.js
+++ script.js
@@ -2,16 +2,49 @@ console.log("Hello, World!");
 
 const customerContainer = document.querySelector("#customer_container")
 
-function buildDirectory(customerArray) {
-    for (let customer of customerArray) {
+
+for (let customer of customers) {
     let customerDiv = document.createElement("div");
-    customerDiv.classList.add("customer"); 
-    let name = document.createElement("p")
-    name.innerText = `${customer.name.title} ${customer.name.first} ${customer.name.last}`;
-    customerDiv.appendChild(name);
+    let customerPic = document.createElement("img");
+    let myName = document.createElement("p");
+    let customerEmail = document.createElement("p");
+    let customerAddressLineOne = document.createElement("p");
+    let customerAddressLineTwo = document.createElement("p");
+    let customerDOB = document.createElement("p")
+    let customerSince = document.createElement("p")
+
+
+
+    customerPic.src = `${customer.picture.medium}`;
+    myName.innerText = `${customer.name.title} ${customer.name.first} ${customer.name.last}`;
+    customerEmail.innerText = `${customer.email}`;
+    customerAddressLineOne.innerText = `${customer.location.street.number} ${customer.location.street.name}`;
+    customerAddressLineTwo.innerText = `${customer.location.city}, ${customer.location.state}`;
+    customerDOB.innerText = `DOB: ${moment.utc(customer.dob.date)}`;
+    customerSince.innerText = `Customer Since: ${moment.utc(customer.registered.date)}`;
+    console.log("custmoer card created");
+
+
+
+
+
+
+    customerDiv.appendChild(customerPic);
+    customerDiv.appendChild(myName);
+    customerDiv.appendChild(customerEmail);
+    customerDiv.appendChild(customerAddressLineOne);
+    customerDiv.appendChild(customerAddressLineTwo);
+    customerDiv.appendChild(customerDOB);
+    customerDiv.appendChild(customerSince);
+
+
     customerContainer.appendChild(customerDiv);
+
+
+    customerDiv.classList.add("customer");
+
+
+
 }
-}
-buildDirectory(customers);
 
 
