eebf302da0d72d6005a6243f3de35f761c9ec1c9
Timcooper709 <112970939+Timcooper709@users.noreply.github.com>
2022-09-28 17:31:30 -0400

    rebuild

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
 console.log("Hello, World!");
 
 const customerContainer = document.querySelector("#customer_container")
+// created a container for my info to populate in//
 
-const customer = "myName";
+function toFirstLetterUpperCase(string){
+    const capitalizedLetters= string.charAt(0).toUpperCase() + string.slice(1);
+    return capitalizedLetters;
+}// Changes all the first letters in the names to capital//
 
 
 for (let customer of customers) {
     let customerDiv = document.createElement("div");
     let customerPic = document.createElement("img");
-    let myName = document.createElement("p");
+    let customerName = document.createElement("p");
     let customerEmail = document.createElement("p");
     let customerAddressLineOne = document.createElement("p");
     let customerAddressLineTwo = document.createElement("p");
     let customerDOB = document.createElement("p")
     let customerSince = document.createElement("p")
-
+    // this loop pulls all the info from customers.js//
 
 
     customerPic.src = `${customer.picture.medium}`;
-    myName.innerText = `${customer.name.title} ${customer.name.first} ${customer.name.last}`;
+    customerName.innerText = `${toFirstLetterUpperCase(customer.name.title)} ${toFirstLetterUpperCase(customer.name.first)} ${toFirstLetterUpperCase(customer.name.last)}`;
+    // add toFirstLetterUpperCase to call the function wrote above to capitalize the names//
     customerEmail.innerText = `${customer.email}`;
     customerAddressLineOne.innerText = `${customer.location.street.number} ${customer.location.street.name}`;
     customerAddressLineTwo.innerText = `${customer.location.city}, ${nameToAbbr(customer.location.state)}`;
+    // added nameToAbbr to shorten state name//
     customerDOB.innerText = `DOB: ${moment(customer.dob.date).format('MMMM Do YYYY')}`;
     customerSince.innerText = `Customer Since: ${moment(customer.registered.date).format('MMMM Do YYYY')}`;
     console.log("custmoer card created");
-    
+    //added moment.format this make the DOB appear in the correct format//
+    // this is how the info will be formated on the html.//
     
 
 
@@ -35,7 +42,7 @@ for (let customer of customers) {
 
 
     customerDiv.appendChild(customerPic);
-    customerDiv.appendChild(myName);
+    customerDiv.appendChild(customerName);
     customerDiv.appendChild(customerEmail);
     customerDiv.appendChild(customerAddressLineOne);
     customerDiv.appendChild(customerAddressLineTwo);
